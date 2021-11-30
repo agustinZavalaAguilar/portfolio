7a5a63795d4eb01ee2d0f45f1c7c5a522a811c73
agustinZavalaAguilar <movil82@gmail.com>
2021-11-30 15:42:56 +0100

    Added javascript effects to links, desktop view

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-/*document.querySelector('header').innerHTML = "<h1>surprise!</h1>";*/
+/*document.getElementById('about').innerHTML = "<h1>surprise!</h1>";*/
 
 /*document.querySelector('h1').style.color = '#201F2E';*/
 
-/*document.querySelector('h1').style.fontFamily = 'serif';
+/*document.querySelector('h1').style.fontFamily = 'serif';*/
+/*----About me effect--------------------------*/
+let aboutTarget = document.getElementById('about');
 
-let eventTarget = document.querySelector('h2');
-
-eventTarget.addEventListener('mouseover', function(){
-    eventTarget.style.color = 'rgb(231, 235, 16)';    
+aboutTarget.addEventListener('mouseover', function(){
+    aboutTarget.innerHTML = 'who I am'; 
+    aboutTarget.style.fontSize = '5em';   
 });
 
-eventTarget.addEventListener('mouseout', function(){
-    eventTarget.style.color = '#fff';    
-});*/
+aboutTarget.addEventListener('mouseout', function(){
+    aboutTarget.innerHTML = 'about';
+    aboutTarget.style.fontSize = '4em';      
+});
+
+/*------------- Work/ Portfolio effect-----------------*/
+let workTarget = document.getElementById('work');
+
+workTarget.addEventListener('mouseover', function(){
+    workTarget.innerHTML = 'portfolio';
+    workTarget.style.fontSize = '5em';    
+});
+
+workTarget.addEventListener('mouseout', function(){
+    workTarget.innerHTML = 'work';
+    workTarget.style.fontSize = '4em';     
+});
+
+/*------------ Contact/ let's talk effect----------*/
+
+let contactTarget = document.getElementById('contact');
+
+contactTarget.addEventListener('mouseover', function(){
+    contactTarget.innerHTML = "let's talk";
+    contactTarget.style.fontSize = '5em';
+
+});
+
+contactTarget.addEventListener('mouseout', function(){
+    contactTarget.innerHTML = "contact";
+    contactTarget.style.fontSize = '4em';     
+});
+
 
