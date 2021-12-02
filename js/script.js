abd6afce2928ce0b41f4ab5b0f79b70965a04ed6
agustinZavalaAguilar <movil82@gmail.com>
2021-12-02 11:09:07 +0100

    Latest tweaks to styling desktop view

--- js/script.js
+++ js/script.js
@@ -6,42 +6,42 @@
 /*----About me effect--------------------------*/
 let aboutTarget = document.getElementById('about');
 
-aboutTarget.addEventListener('mouseover', function(){
-    aboutTarget.innerHTML = 'this is me'; 
-    aboutTarget.style.fontSize = '5em';   
+aboutTarget.addEventListener('mouseenter', function(){
+    aboutTarget.innerHTML = 'this is me!'; 
+    /*aboutTarget.style.fontSize = '5em';   */
 });
 
 aboutTarget.addEventListener('mouseout', function(){
     aboutTarget.innerHTML = 'about';
-    aboutTarget.style.fontSize = '4em';      
+    /*aboutTarget.style.fontSize = '4em';      */
 });
 
 /*------------- Work/ Portfolio effect-----------------*/
 let workTarget = document.getElementById('work');
 
-workTarget.addEventListener('mouseover', function(){
-    workTarget.innerHTML = 'portfolio';
-    workTarget.style.fontSize = '5em';    
+workTarget.addEventListener('mouseenter', function(){
+    workTarget.innerHTML = 'portfolio!';
+    /*workTarget.style.fontSize = '5em';    */
 });
 
 workTarget.addEventListener('mouseout', function(){
     workTarget.innerHTML = 'work';
-    workTarget.style.fontSize = '4em';     
+    /*workTarget.style.fontSize = '4em';     */
 });
 
 /*------------ Contact/ let's talk effect----------*/
 
 let contactTarget = document.getElementById('contact');
 
-contactTarget.addEventListener('mouseover', function(){
-    contactTarget.innerHTML = "let's talk";
-    contactTarget.style.fontSize = '5em';
+contactTarget.addEventListener('mouseenter', function(){
+    contactTarget.innerHTML = "let's talk!";
+    /*contactTarget.style.fontSize = '5em';*/
 
 });
 
 contactTarget.addEventListener('mouseout', function(){
     contactTarget.innerHTML = "contact";
-    contactTarget.style.fontSize = '4em';     
+    /*contactTarget.style.fontSize = '4em';     */
 });
 
 
